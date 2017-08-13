8f1930d4b97d42fefa9f1487dee6c27ad439a9c3
Julien Duseyau <julien.duseyau@gmail.com>
2017-08-13 18:40:52 +0200

    Preparing tests

--- tests/ExampleTest.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-namespace Maduser\Minimal\Translation\Tests;
-
-class ExampleTest extends \PHPUnit_Framework_TestCase
-{
-
-}
---
+++ tests/TranslationTest.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Maduser\Minimal\Translation\Tests;
+
+use PHPUnit\Framework\TestCase;
+
+class TranslationTest extends TestCase
+{
+
+}
